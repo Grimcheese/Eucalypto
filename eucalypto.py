a4c2ad025f37beb6d38f336c5c186ddaf50e8f4a
Alby Chawula <albychawula@gmail.com>
2024-07-17 19:59:49 +0800

    Add end routes for main pages that will be used

--- eucalypto.py
+++ eucalypto.py
@@ -1,4 +1,27 @@
 """Start point for Eucalypto web server"""
 
-import flask
+from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template("home.html")
+
+
+@app.route('/login/')
+def login_page():
+    return render_template("login.html")
+
+@app.route('/species/')
+def species_page():
+    return render_template("species_list.html")
+
+@app.route("/spaces/")
+def spaces_page():
+    return render_template("spaces.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
